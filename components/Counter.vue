9227d6484321ab2dbc4bdbd8e4fd830372e56dfc
yuki takagi <yuki99takagi@gmail.com>
2020-10-19 20:37:29 +0900

    テンプレートの作成

---
+++ components/Counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    counter: {{ $store.state.counter.counter}}
+    <button v-on:click="$store.commit('counter/countUp')">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      title: 'Counter App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
